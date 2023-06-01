7b0dc575e8b2e9b493ee9c6e514402fe36786e42
anshawasthi01 <anshawasthi3894@gmail.com>
2023-06-01 13:51:03 +0530

    component completed without context(context used for dynamic cart value

--- melody-market/pages/_app.js
+++ melody-market/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
+
+export default MyApp
--- melody-market/pages/index.js
+++ melody-market/pages/index.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-const Home = () => {
-  return (
-    <>
-      Hero Banner
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
-      <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
 
-      <div className="products-container">
-        {['Product 1', 'Product 2'].map((product) => product)}
-      </div>
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
 
-      Footer
-    </> 
-  )
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
+
+
+// grab all the product from sanity
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
-export default Home
+export default Home;
+
