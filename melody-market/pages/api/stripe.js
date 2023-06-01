7791269c526cf2de79b3d38ecbb7073c963bf58a
anshawasthi01 <anshawasthi3894@gmail.com>
2023-06-01 15:50:13 +0530

    stripe api

--- melody-market/pages/api/stripe.js
+++ melody-market/pages/api/stripe.js
@@ -36,7 +36,6 @@ export default async function handler(req, res) {
         success_url: `${req.headers.origin}/success`,
         cancel_url: `${req.headers.origin}/canceled`,
       }
-
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
 
